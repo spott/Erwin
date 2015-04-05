ccba4c91d4ff649bd4cff753f2ea7fd8c1bcc5ed
Andrew Spott <andrew.spott@gmail.com>
2015-04-05 00:18:44 -0600

    laser observable

--- include/parameters/laser.hpp
+++ include/parameters/laser.hpp
@@ -2,6 +2,7 @@
 
 #include <boost/program_options.hpp>
 #include <utilities/math.hpp>
+#include <time_dependent/observables.hpp>
 #include <functional>
 #include <string>
 #include <fstream>
@@ -11,7 +12,7 @@ namespace Erwin
 {
 
 struct LaserEnvelope {
-    virtual double operator()( double t ) const = 0;
+    virtual double operator()( double t, double frequency ) const = 0;
     virtual std::string print() const = 0;
     virtual boost::program_options::options_description options() = 0;
     virtual ~LaserEnvelope();
@@ -19,13 +20,42 @@ struct LaserEnvelope {
 
 
 struct sin_squared final : LaserEnvelope {
-    double operator()( double t ) const;
+    double operator()( double t, double frequency ) const;
     std::string print() const;
     boost::program_options::options_description options();
-    // virtual ~sin_squared() {}
 
     double intensity;
-    double period;
+    double cycles;
+};
+
+struct EfieldObserver final : Observable {
+    EfieldObserver( std::string folder_,
+                    const std::function<double(double)>& ef )
+        : draw( 1, PETSC_COMM_WORLD ), folder( folder_ ), current_value( 0 ),
+          efield( ef )
+    {
+        draw.set_title("Efield");
+        outfile.open( folder + "/efield.dat", std::ios_base::binary |
+                                                  std::ios_base::app |
+                                                  std::ios_base::out );
+    }
+    void
+    operator()(const petsc::Matrix& A,const petsc::Vector& U, petsc::TimeStepper& ts );
+
+    void modify(petsc::Matrix&, petsc::Vector&,petsc::Vector&, petsc::TimeStepper&) {}
+
+    std::string last() const;
+    std::string name() const;
+
+  private:
+    petsc::Draw draw;
+    std::string folder;
+    std::ofstream outfile;
+    double current_value;
+    const std::function<double(double)>& efield;
+    double next_value{0};
+    bool interpolate_next{false};
+    unsigned zero{0};
 };
 
 struct LaserParameters {
@@ -34,26 +64,33 @@ struct LaserParameters {
                      double cep_,
                      std::string folder_,
                      std::shared_ptr<LaserEnvelope> envelope_ )
-        : frequency( frequency_ ), cep( cep_ ), folder( folder_ ),
-          envelope( envelope_ )
+        : frequency( frequency_ ), cep( cep_ ), envelope( envelope_ ),
+          folder( folder_ ),
+          ef( [this]( double t ) {
+              return ( *this->envelope.get() )( t, this->frequency ) *
+                     sin( this->frequency * t + this->cep );
+          } )
     {
     }
 
-    std::function<double(double)> efield() const
+    const std::function<double(double)>& efield() const
     {
         using namespace std;
-        return [&]( double t ) {
-            return ( *envelope )(t)*sin( frequency * t + cep );
-        };
+        return ef;
     }
+
+    std::unique_ptr<Observable> get_observer() const;
 
     std::string print() const;
     void write() const;
 
     double frequency;
     double cep;
-    std::string folder;
     std::shared_ptr<LaserEnvelope> envelope;
+
+  private:
+    std::string folder;
+    const std::function<double(double)> ef;
 };
 
 
--- src/parameters/laser.cpp
+++ src/parameters/laser.cpp
@@ -4,13 +4,15 @@
 namespace Erwin
 {
 
+// to kill the weak vtable error... need to figure out why that happens
 LaserEnvelope::~LaserEnvelope() {}
 
-double sin_squared::operator()( double t ) const
+double sin_squared::operator()( double t, double frequency ) const
 {
     using namespace std;
-    return t < period
-               ? intensity * pow( sin( math::PI<double> * t / period ), 2. )
+    return ( t < ( cycles * 2. * math::PI / frequency ) and t > 0 )
+               ? sqrt( intensity ) *
+                     pow( sin( frequency * t / ( cycles * 2. ) ), 2. )
                : 0.0;
 }
 
@@ -18,7 +20,7 @@ std::string sin_squared::print() const
 {
     using namespace std;
     stringstream ss;
-    ss << "laser_sin_squared_period=" << period << endl;
+    ss << "laser_sin_squared_cycles=" << cycles << endl;
     ss << "laser_sin_squared_intensity=" << intensity << endl;
     return ss.str();
 }
@@ -27,11 +29,11 @@ boost::program_options::options_description sin_squared::options()
 {
     using namespace boost::program_options;
     options_description v( "Sin Squared Laser Pulse Options" );
-    v.add_options()( "laser_sin_squared_period",
-                     value<double>( &period )->default_value( 10 ),
-                     "the period for a sin squared laser pulse" )(
+    v.add_options()( "laser_sin_squared_cycles",
+                     value<double>( &cycles )->default_value( 10 ),
+                     "the total period for a sin squared laser pulse" )(
         "laser_sin_squared_intensity",
-        value<double>( &intensity )->default_value( 1e14 ),
+        value<double>( &intensity )->default_value( .001 ),
         "the intensity for a sin squared laser pulse" );
     return v;
 }
@@ -42,8 +44,69 @@ std::string LaserParameters::print() const
     stringstream ss;
     ss << "laser_frequency=" << frequency << endl;
     ss << "laser_cep=" << cep << endl;
+    ss << "propagate_folder=" << folder << endl;
     ss << envelope->print();
     return ss.str();
+}
+
+void EfieldObserver::
+operator()( const petsc::Matrix&, const petsc::Vector&, petsc::TimeStepper& ts )
+{
+    if ( interpolate_next ) {
+        // find the time where ef(t) ~= 0:
+        double tnext = ts.time();
+        double tcurrent = ts.time() - ts.dt();
+        double t = ( 2 * ts.time() - ts.dt() ) / 2.;
+        while ( std::abs( efield( t ) ) > 1e-12 and tnext - t > 1e-16 and
+                t - tcurrent > 1e-16 ) {
+            // if sign(ef(t)) != sign(current_value) then we haven't gone
+            // far enough.  advance half the distance to the next timestep
+            if ( math::signum( efield( t ) ) ==
+                 math::signum( current_value ) ) {
+                auto tmp = t;
+                t = ( tnext + t ) / 2.;
+                tcurrent = tmp;
+            }
+            // else, we have gone to far, set t to miday between current
+            // timestep and t;
+            else {
+                auto tmp = t;
+                t = ( tcurrent + t ) / 2.;
+                tnext = tmp;
+            }
+        }
+
+        ts.interpolate( t )
+            .to_file( folder + "/wf_" + std::to_string( zero ) + ".dat" );
+        zero++;
+        interpolate_next = false;
+    }
+    current_value = next_value;
+    next_value = efield( ts.time() + ts.dt() );
+
+    if ( math::signum( current_value ) != math::signum( next_value ) &&
+         math::signum( current_value ) != 0 ) {
+        interpolate_next = true;
+    }
+
+    outfile << current_value;
+    double t = ts.time();
+    draw.draw_point( &t, &current_value );
+}
+
+std::string EfieldObserver::last() const
+{
+    using namespace std;
+    char buffer[12];
+    std::snprintf( &buffer[0], 12, "%9.3e", current_value );
+    return std::string( buffer );
+}
+
+std::string EfieldObserver::name() const { return "efield"; }
+
+std::unique_ptr<Observable> LaserParameters::get_observer() const
+{
+    return std::unique_ptr<Observable>( new EfieldObserver( folder, ef ) );
 }
 
 void LaserParameters::write() const
@@ -72,7 +135,7 @@ const LaserParameters make_LaserParameters( int argc, const char** argv )
         ( "laser_cep", po::value<double>()->default_value( 0 ),
           "the carrier envelope phase" )
 
-            ( "laser_folder", po::value<string>()->default_value( "./" ),
+            ( "propagate_folder", po::value<string>()->default_value( "./" ),
               "the folder the various laser files should be written "
               "in" )
 
@@ -97,30 +160,30 @@ const LaserParameters make_LaserParameters( int argc, const char** argv )
     ifstream fs( config_filename );
     vm.clear();
 
-    po::store( po::parse_config_file( fs, laser, true ), vm );
     po::store( po::command_line_parser( argc, argv )
                    .options( laser )
                    .allow_unregistered()
                    .run(),
                vm );
+    po::store( po::parse_config_file( fs, laser, true ), vm );
 
     shared_ptr<LaserEnvelope> ptr;
 
     if ( vm["laser_envelope_shape"].as<string>() == "sin_squared" ) {
         ptr.reset( new sin_squared() );
         auto sins = ptr->options();
-        po::store( po::parse_config_file( fs, sins, true ), vm );
         po::store( po::command_line_parser( argc, argv )
                        .options( sins )
                        .allow_unregistered()
                        .run(),
                    vm );
+        po::store( po::parse_config_file( fs, sins, true ), vm );
     }
     po::notify( vm );
 
 
     return LaserParameters( vm["laser_frequency"].as<double>(),
                             vm["laser_cep"].as<double>(),
-                            vm["laser_folder"].as<string>(), ptr );
+                            vm["propagate_folder"].as<string>(), ptr );
 }
 }
